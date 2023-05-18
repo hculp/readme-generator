a22c7f4d7d9fce7ac9fdfd465727e9f812b3ab97
Houston Culpepper <howacul@gmail.com>
2023-05-18 14:48:57 -0400

    added questions for user input

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is your project title?",
+    "What is the discription for your project?",
+    "What are the installation instructions?",
+    "What is the usage information?",
+    "What are the contribution guidelines?", 
+    "What are the tests instructions?",
+    "Which license would you like to use?", 
+    "What is your associated GitHub username?",
+    "What email would you like to use so that others can reach out you about question on this project?"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
